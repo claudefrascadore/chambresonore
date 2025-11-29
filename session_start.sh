1d9e3fcc6a37a43d69ffb48a440eaabf2e28eb8b
Claude Frascadore <ton-email@exemple.com>
2025-11-29 16:22:19 -0500

    Ajout script session_start.sh

---
+++ session_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Lance la Chambre Sonore depuis n'importe quel répertoire
+
+cd ~/Projets/ChambreSonore
+source ~/Projets/ChambreSonore/.venv/bin/activate
+python3 ~/Projets/ChambreSonore/src/main.py
+
